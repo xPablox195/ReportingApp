8c6971511f2e7c736868e9322ced44792c67a5f6
pluparia <lupariapablo@gmail.com>
2021-05-14 20:27:37 -0300

    Export Document .CSV
    Se puede exportar el documento en el formato csv

--- src/app/pages/xls/xls.page.ts
+++ src/app/pages/xls/xls.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Papa } from 'ngx-papaparse';
+import { Platform } from '@ionic/angular';
+import { File } from '@ionic-native/file/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-xls',
@@ -12,7 +15,8 @@ export class XlsPage implements OnInit {
   csvData: any[] = [];
   headerRow: any[] = [];
 
-  constructor(private http: HttpClient, private papa: Papa) {
+  constructor(private http: HttpClient, private papa: Papa, private plt: Platform, private socialSharing: SocialSharing,
+    private file: File) {
     this.loadCSV();
    }
 
@@ -41,4 +45,35 @@ export class XlsPage implements OnInit {
   ngOnInit() {
   }
 
+  exportCSV(){
+    let csv = this.papa.unparse({
+      fields: this.headerRow,
+      data: this.csvData
+    });
+    if (this.plt.is('cordova')) {
+      this.file.writeFile(this.file.dataDirectory, 'data.csv', csv, {replace: true}).then( res => {
+        this.socialSharing.share(null, null, res.nativeURL, null).then(e =>{
+          // Success
+        }).catch(e => {
+          console.log('Share failed:', e);
+        });
+      }, err => {
+        console.log('Error: ', err);
+      });
+ 
+    } else {
+      // Dummy implementation for Desktop download purpose
+      var blob = new Blob([csv]);
+      var a = window.document.createElement('a');
+      a.href = window.URL.createObjectURL(blob);
+      a.download = 'newdata.csv';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  }
+ 
+  trackByFn(index: any, item: any) {
+    return index;
+  }
 }
